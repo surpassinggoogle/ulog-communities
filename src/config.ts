1132765cfb89e76acade282f77473cc2976a943c
East <eastmaels@gmail.com>
2018-10-19 00:42:55 +0900

    all filters done

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,9 @@
 const BOT_COMMAND = 'ulogy'
-const MAIN_TAG = 'ulogs'
+const MAIN_TAG = 'ulog'
 const ULOGS_APP = 'ulogs/1.0.0'
-const SUB_TAGS: string[] = ['ulog-cat1', 'ulog-cat2', 'ulog-cat3', 'ulog-cat4']
 const CERTIFIED_ULOGGERS: string[] = ['east.autovote', 'eastmael']
 const OVERSEERS = [
-  {cat: 'ulog-cat1', overseers: ['east.autovote', 'eastmael']}, 
-  {cat: 'ulog-cat2', overseers: ['east.autovote']}, 
-  {cat: 'ulog-cat3', overseers: ['eastmael']}, 
-  {cat: 'ulog-cat4', overseers: []}, 
+  {name: 'east.autovote', 'tags': ['surpassinggoogle', 'ulog-cat2']}, 
 ]
 
 const COMMENT = (author: string): string => {
@@ -22,4 +18,4 @@ const WHITELIST: string[] = ['superoo7']
 // percentage allowed for CN words (e.g. 20%)
 const PERCENTAGE: number = 20
 
-export { ULOGS_APP, BOT_COMMAND, MAIN_TAG, SUB_TAGS, CERTIFIED_ULOGGERS, OVERSEERS, COMMENT, WHITELIST, PERCENTAGE }
+export { ULOGS_APP, BOT_COMMAND, MAIN_TAG, CERTIFIED_ULOGGERS, OVERSEERS, COMMENT, WHITELIST, PERCENTAGE }
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as util from 'util'
 
 // file
 import { arrayContains, die } from './functions'
-import { ULOGS_APP, BOT_COMMAND, MAIN_TAG, CERTIFIED_ULOGGERS } from './config'
+import { ULOGS_APP, BOT_COMMAND, MAIN_TAG, CERTIFIED_ULOGGERS, OVERSEERS } from './config'
 import { getContent, getPostData, comment, getCertifiedUloggers } from './steem'
 
 // Init
@@ -40,7 +40,6 @@ stream.on('data', async operation => {
     // 2) check if certified ulogger
     let author: string = txData.author
     if(!arrayContains(author, CERTIFIED_ULOGGERS)) return
-    console.log('is certified ulogger', arrayContains(author, CERTIFIED_ULOGGERS))
 
     // get post content/body
     let permlink: string = txData.permlink
@@ -58,16 +57,50 @@ stream.on('data', async operation => {
     }
     if(app.indexOf(ULOGS_APP) < 0) return
 
-    // 4) post contains specific command
-    if (post.body && post.body.indexOf(BOT_COMMAND) >= 0) {
-      console.log('sendingComment')
-      // Send Comment
-      /*
-      comment(client, author, permlink, key, ACCOUNT_NAME).catch(() =>
-        console.error("Couldn't comment on the violated post")
-      )
-      /* */
+    // 4) body contains specific command
+    if (post.body && post.body.indexOf(BOT_COMMAND) < 0) return
+
+    let tags: string[]
+    try {
+      tags = JSON.parse(post.json_metadata).tags
+    } catch (e) {
+      console.error('Invalid tags')
+      return
     }
+
+    // 5) First tag is 'ulogs'
+    if (tags[0] !== MAIN_TAG) return
+
+    // 6)get root post (to get all tags)
+    let rootPost = await getPostData(post.parent_author, post.parent_permlink).catch(() =>
+      console.error("Couldn't fetch ROOT post data with SteemJS")
+    )
+
+    // 7) Summoner is overseer of sub-tag
+    let rootTags: string[]
+    try {
+      rootTags = JSON.parse(rootPost.json_metadata).tags
+    } catch (e) {
+      console.error('Invalid root tags')
+      return
+    }
+    // TODO: change to map
+    let subtags = OVERSEERS.filter(overseerObj => overseerObj.name === author).map(result => result.tags)
+    // 7a) Is an overseer?
+    console.log('is an overseer? ', subtags);
+    if (subtags && subtags.length == 0) return
+
+    // 7b) Is an overseer of sub-tag?
+    console.log('is an overseer of sub-tag? ', arrayContains(rootTags[1], subtags[0]));
+    if(!arrayContains(rootTags[1], subtags[0])) return
+
+    console.log('sendingComment')
+    // Send Comment
+    /*
+    comment(client, author, permlink, key, ACCOUNT_NAME).catch(() =>
+      console.error("Couldn't comment on the violated post")
+    )
+    /* */
   }
   return
 })
