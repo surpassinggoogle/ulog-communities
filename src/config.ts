864e1fc6f540fbd7897bf3758f8f4f9a68ec40e1
East <eastmaels@gmail.com>
2018-10-27 07:47:41 +0900

    added subtag overseers using config file (temp solution)

--- src/config.ts
+++ src/config.ts
@@ -10,9 +10,155 @@ interface ConditionFlags {
 }
 
 let OVERSEERS: {[key:string]: any} = {
-  'east.autovote': { tags: ['surpassinggoogle', 'subtag'], maxweight: 50}, 
-  'eastmael': { tags: ['surpassinggoogle', 'newsubtag'], maxweight: 60}, 
-  'surpassinggoogle': { tags: ['surpassinggoogle'], maxweight: 100}, 
+  'east.autovote': {
+    tags: [
+      'subtag',
+    ],
+    maxweight: 50
+  }, 
+  'eastmael': {
+    tags: [
+      'newsubtag',
+    ],
+    maxweight: 60
+  }, 
+  'ankarlie': {
+    tags: [
+      'ulog-blocktrades',
+    ],
+    maxweight: 100
+  }, 
+  'bloghound': {
+    tags: [
+      'ulog-gratefulvibes',
+    ],
+    maxweight: 100
+  },
+  'iyanpol12': {
+    tags: [
+      'ulog-bobbylee',
+    ],
+    maxweight: 100
+  },
+  'kusknee': {
+    tags: [
+      'ulog-kusknee',
+    ],
+    maxweight: 100
+  },
+  'mermaidvampire': {
+    tags: [
+      'ulog-showerthoughts',
+    ],
+    maxweight: 100
+  },
+  'papa-pepper': {
+    tags: [
+      'ulog-papapepper',
+    ],
+    maxweight: 100
+  },
+  'paradise-found': {
+    tags: [
+      'ulog-gratefulvibes',
+    ],
+    maxweight: 100
+  },
+  'shadowspub': {
+    tags: [
+      'ulog-snookmademedoit',
+    ],
+    maxweight: 100
+  },
+  'snook': {
+    tags: [
+      'ulog-snookmademedoit',
+    ],
+    maxweight: 100
+  },
+  'stellabelle': {
+    tags: [
+      'ulogifs',
+      'ulog-stellabelle',
+    ],
+    maxweight: 100
+  },
+  'sweetsssj': {
+    tags: [
+      'ulog-sweetsssj',
+    ],
+    maxweight: 100
+  },
+  'surfyogi': {
+    tags: [
+      'ulog-surfyogi',
+    ],
+    maxweight: 100
+  },
+  'surpassinggoogle': {
+    tags: [
+      'surpassinggoogle',
+      'ulog-quotes',
+      'ulog-howto',
+      'ulog-diy',
+      'ulog-surpassinggoogle',
+      'teardrops',
+      'untalented',
+      'ulog-ned',
+      'ulography',
+      'ulog-gratefulvibes',
+      'ulog-resolutions',
+      'ulog-memes',
+      'ulog-blocktrades',
+      'ulog-showerthoughts',
+      'ulog-snookmademedoit',
+      'ulog-utopian',
+      'ulog-thejohalfiles',
+      'ulogifs',
+      'ulog-surfyogi',
+      'ulog-bobbylee',
+      'ulog-stellabelle',
+      'ulog-sweetsssj',
+      'ulog-dimimp',
+      'ulog-teamsteem',
+      'ulog-kusknee',
+      'ulog-papapepper',
+    ],
+    maxweight: 100
+  },
+  'samic': {
+    tags: [
+      'ulog-quotes',
+      'ulog-memes',
+    ],
+    maxweight: 100
+  }, 
+  'sunnylife': {
+    tags: [
+      'ulography',
+    ],
+    maxweight: 100
+  },
+  'teamsteem': {
+    tags: [
+      'ulog-teamsteem',
+    ],
+    maxweight: 100
+  }, 
+  'uche-nna': {
+    tags: [
+      'ulog-steemjet',
+    ],
+    maxweight: 100
+  },
+  'ulogs': {
+    tags: [
+      'ulog-kusknee',
+      'ulog-papapepper',
+      'ulog-steemjet',
+    ],
+    maxweight: 100
+  },
 }
 
 const FAIL_COMMENT = (author: string, bot: string, subtag: string, 
